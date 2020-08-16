0edb2633a096d324f7acc65f67536cba070af0af
Fatih <fatih16065@gmail.com>
2020-08-16 14:19:32 +0300

    ikinci satir ekledim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -3,7 +3,9 @@ package git_project_ikinci;
 public class Deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("2.git projem");
+		System.out.println("2.Satir kodum");
 
 	}
 
